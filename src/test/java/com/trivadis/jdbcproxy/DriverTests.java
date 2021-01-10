2c3b8097fd3d500fa68e8f88c6418de4ada7076a
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-01-11 00:32:30 +0100

    add test cases using the old MySQL driver directly

--- src/test/java/com/trivadis/jdbcproxy/DriverTests.java
+++ src/test/java/com/trivadis/jdbcproxy/DriverTests.java
@@ -17,7 +17,6 @@
 package com.trivadis.jdbcproxy;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
@@ -60,7 +59,30 @@ public class DriverTests {
     }
 
     @Test
-    @Disabled
+    void mySQL_with_old_Driver() throws SQLException {
+        SingleConnectionDataSource dataSource = new SingleConnectionDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/mysql");
+        dataSource.setUsername("root");
+        dataSource.setPassword("mysql");
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        String actual = jdbcTemplate.queryForObject("select 'Hello World!' as test", String.class);
+        Assertions.assertEquals("Hello World!", actual);
+    }
+
+    @Test
+    void mySQL_with_old_Driver_to_PostgreSQL() throws SQLException {
+        SingleConnectionDataSource dataSource = new SingleConnectionDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://jdbc:postgresql://localhost:5432/postgres:/SomeDbToRemove");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("postgres");
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        String actual = jdbcTemplate.queryForObject("select 'Hello World!' as test", String.class);
+        Assertions.assertEquals("Hello World!", actual);
+    }
+
+    //@Test
     void jdbcProxy_to_Snowflake() throws SQLException {
         SingleConnectionDataSource dataSource = new SingleConnectionDataSource();
         dataSource.setUrl("jdbc:proxy:jdbc:snowflake://<account>.snowflakecomputing.com:443");
